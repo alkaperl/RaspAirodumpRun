408febc98e8987bff0d23c81abe41b95cd706a22
Xavid Ramirez <xavid.ramirez01@utrgv.edu>
2016-06-20 10:23:17 -0500

    Testing push to actual database

--- AirodumpStart.py
+++ AirodumpStart.py
@@ -3,18 +3,73 @@ import os
 import csv
 import pexpect
 import time
-import subprocess
 import datetime
+import requests
+import json
+import commands
+import urllib
 
-Time = 36000
+url = "http://45.55.165.42/Device"
 
 def airodumpStart():
+	print "Startgin up Airodump-ng:"
+	print "\tInterface : Wlan1mon"
+	print "\tFileType: csv"
+	print "\tFilename: data-01.csv"
 	cmd_airodump = pexpect.spawn('airodump-ng wlan1mon -w data')
-	cmd_airodump.expect([pexpect.TIMEOUT, pexpect.EOF], 10)
+	cmd_airodump.expect([pexpect.TIMEOUT, pexpect.EOF], 30)
+	print "Airodump-ng Stopping..."
+	print "Saving Airodump-ng contents.."
 	cmd_airodump.close()
+	print "Save complete! --"
 
+def getMAC(MACADDRESS):
+	#one api   http://searchmac.com/api/raw/
+	#second api   http://macvendors.co/api/
+	#third option http://api.macvendors.com/
+	API = 'http://api.macvendors.com/%s'
+	vendor = urllib.urlopen(API % MACADDRESS).read()
+	if len(vendor) > 1:
+		return vendor
+	else:
+		return "Unkown"
+
+def myMAC(iface):
+	words = commands.getoutput("ifconfig" + iface).split()
+	if "HWaddr" in words:
+		return words[words.index("HWaddr") + 1]
+	else:
+		return 'NULL'
 
 ###ScriptStart##
 #airodumpStart()
 
-print datetime.datetime.now()
+while True:
+	print "Script starting....\n"
+
+	print  "Script initiated at : %s" % (datetime.datetime.now())
+	print "Please do not interupt the script...\n"
+
+	airodumpStart()
+
+	print "Processing the file... please wait."
+	print "Pushing to database..."
+
+
+	with open('data-01.csv' , 'rb') as csvfile:
+		lines = csv.reader(csvfile)
+		lines.next()
+		for line in lines:
+			if len(line) > 1:
+				if "Station" in line[0]:
+					lines.next()
+					for line in lines:
+						if len(line) > 1:
+							payload = {'node' : myMac("etho") , 'mac' : line[0] , 'firstseen': line[1] , 'lastseen' : line[2], 'company' : getMAC(line[0]) }
+							r = requests.post(url , params=payload)
+
+	print "Successfully pushed to database"
+	print "removing csv file"
+	os.remove('data-01.csv')
+	print "Script completed at: %s" % (datetime.datetime.now())
+	
