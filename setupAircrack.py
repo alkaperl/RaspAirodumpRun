0c36a5195f8e24ff9ccae96b9a612608c280a453
xavid Ramirez <xavid.ramirez01@utrgv.edu>
2016-06-24 16:27:08 -0500

    modified setup

--- setupAircrack.py
+++ setupAircrack.py
@@ -2,14 +2,15 @@ import os, subprocess
 
 subprocess.call(["sudo" , "apt-get" , "-y" , "update"])
 subprocess.call(["sudo" , "apt-get" , "-y" , "upgrade"])
-subprocess.call(["sudo","apt-get","-y" ,"install" , "libssl-dev" ,"libnl-dev" , "git" , "rfkill" , "iw" , "pip-python"])
-subprocess.call(["sudo" , "pip" , "install" , "pip"])
+subprocess.call(["sudo","apt-get","-y" ,"install" , "libssl-dev" ,"libnl-dev" , "git" , "rfkill" , "iw" , "python-pip"])
+subprocess.call(["sudo" , "pip" , "install" , "pexpect"])
 subprocess.call(["wget" , "http://download.aircrack-ng.org/aircrack-ng-1.2-rc4.tar.gz"])
 subprocess.call(["tar", "-zxvf" , "aircrack-ng-1.2-rc4.tar.gz"])
 subprocess.call(["rm" , "aircrack-ng-1.2-rc4.tar.gz"])
 subprocess.call(["make", "-C" , "aircrack-ng-1.2-rc4/"])
-subprocess.call(["sudo" , "make" , "install" , "&&" , "sudo" , "airodump-ng-oui-update"])
-subprocess.call(["rm" , "aircrack-ng-1.2-rc4/"])
+subprocess.call(["sudo" , "make" , "install" ])
+subprocess.call(["sudo" , "airodump-ng-oui-update"])
+subprocess.call(["sudo","rm" , "-rf","aircrack-ng-1.2-rc4/"])
 
 #install finished at this point.
 
